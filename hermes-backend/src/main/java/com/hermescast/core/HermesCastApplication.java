34444b6860d00685acb582604782d18a7c915879
toto49 <tomboudaud0105@gmail.com>
2026-06-06 17:54:19 +0200

    Structure: Initialisation du monorepo (backend et frontend)

---
+++ hermes-backend/src/main/java/com/hermescast/core/HermesCastApplication.java
@@ -0,0 +1,13 @@
+package com.hermescast.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HermesCastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HermesCastApplication.class, args);
+    }
+
+}
---
+++ hermes-backend/src/test/java/com/hermescast/core/HermesCastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hermescast.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HermesCastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
